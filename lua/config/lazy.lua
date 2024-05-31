9dced8f99ca48e1b3d694b173736bec76cb7cd7a
youfa <vsyfar@gmail.com>
2024-05-31 14:07:25 +0800

    feat: 🎸 remove symbols-outline from lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    --{ import = "lazyvim.plugins.extras.editor.symbols-outline" },
     -- import/override with your plugins
     { import = "plugins" },
   },
